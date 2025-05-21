bfb2c2be2694d2f50d12c06b30ec78555f4eccea
Ecce Gratum <212116150+ecce-gratum@users.noreply.github.com>
2025-05-21 15:39:41 +0200

    Update tf2c_sfm.bat

--- tf2c_sfm.bat
+++ tf2c_sfm.bat
@@ -21,7 +21,7 @@ GOTO MAIN
 		)
 	)
 	TITLE TF2C_SFM - ERROR
-	ECHO Could not find a %~1 installation.
+	ECHO:Could not find a %~1 installation.
 	PAUSE
 EXIT 1
 
@@ -33,12 +33,12 @@ GOTO :EOF
 :VPK_SIZE
 	REM 32 bit integer limitations disallows easy conversion to GB
 	FOR /F "tokens=2,3" %%I IN ('DIR "%~1\*.vpk"') DO IF "x%%I" == "xFile(s)" SET RETURN=%%J
-	REM FOR /F "tokens=1" %I IN ('WHERE /T "E:\Steam\steamapps\common\Team Fortress 2\tf:tf2_*.vpk"') DO ECHO %I
+	REM FOR /F "tokens=1" %I IN ('WHERE /T "E:\Steam\steamapps\common\Team Fortress 2\tf:tf2_*.vpk"') DO ECHO:%I
 GOTO :EOF
 
 :VPK_CONFIRM
 	CALL :VPK_SIZE "%~dp1"
-	ECHO This requires %RETURN% bytes of free space.
+	ECHO:This requires %RETURN% bytes of free space.
 	ECHO/
 	ECHO Do you want to continue^? [y/n]
 	SET /P "QUERY=> "
@@ -58,14 +58,14 @@ EXIT 1
 	REM SFM only understands xyz
 	TITLE TF2C_SFM - Fixing Materials...
 	ECHO Sit back^, this can take a few minutes...
-	FOR /F %%I IN ('WHERE /R %1\materials *.vmt') DO (
+	FOR /F %%I IN ('WHERE /R %~1\materials *.vmt') DO (
 		(FOR /F "delims=" %%J IN (%%I) DO (
 			SET LINE=%%J
 			SET LINE=!LINE:"SDK_="!
 			ECHO:!LINE!
 	))>"%%I.FIX.TMP
 		MOVE /Y %%I.FIX.TMP %%I >NUL
-		ECHO Fixed %%I
+		ECHO:Fixed %%I
 	)
 GOTO :EOF
 
@@ -122,31 +122,31 @@ ECHO 1 - Install/Update TF2Classic SFM content
 ECHO 2 - Update TF2 SFM content
 ECHO 3 - Fix TF2Classic SFM installation
 SET /P "QUERY=> "
-IF "x%QUERY%" == "x1" (
-	IF NOT EXIST %DIR_SFM_TF2C%\* (
-		MKDIR %DIR_SFM_TF2C%
+IF "x!QUERY!" == "x1" (
+	IF NOT EXIST "!DIR_SFM_TF2C!\*" (
+		MKDIR "!DIR_SFM_TF2C!"
 		IF %ERRORLEVEL% == 1 (
 			TITLE TF2C_SFM - ERROR
-			ECHO Could not create %DIR_SFM_TF2C%.
+			ECHO:Could not create !DIR_SFM_TF2C!.
 			PAUSE
 			EXIT 1
 		)
-	) ELSE ECHO Updating %DIR_SFM_TF2C% will override files.
-	CALL :VPK_CONFIRM %DIR_TF2C%\vpks\tf2c_assets_dir.vpk
-	CALL :VPK_EXTRACT %DIR_TF2C%\vpks\tf2c_assets_dir.vpk %DIR_SFM_TF2C%
-	CALL :FIXMAT %DIR_SFM_TF2C%
+	) ELSE ECHO:Updating !DIR_SFM_TF2C! will override files.
+	CALL :VPK_CONFIRM "%DIR_TF2C%\vpks\tf2c_assets_dir.vpk"
+	CALL :VPK_EXTRACT "%DIR_TF2C%\vpks\tf2c_assets_dir.vpk" "%DIR_SFM_TF2C%"
+	CALL :FIXMAT "%DIR_SFM_TF2C%"
 ) ELSE (IF "x%QUERY%" == "x2" (
 	CALL :GETDIR "Team Fortress 2"
 	SET DIR_TF=!RETURN!
-	ECHO TF2: !DIR_TF!
-	ECHO Updating %DIR_SFM%\game\tf will override files.
+	ECHO:TF2: !DIR_TF!
+	ECHO:Updating !DIR_SFM!\game\tf will override files.
 	CALL :VPK_CONFIRM "!DIR_TF!\tf\tf2_*_dir.vpk"
-	FOR %%I IN (sound_misc misc textures) DO CALL :VPK_EXTRACT "!DIR_TF!\tf\tf2_%%I_dir.vpk" %DIR_SFM%\game\tf
+	FOR %%I IN (sound_misc misc textures) DO CALL :VPK_EXTRACT "!DIR_TF!\tf\tf2_%%I_dir.vpk" "%DIR_SFM%\game\tf"
 	ECHO/
 	ECHO All done!
 	GOTO ALLDONE
 ) ELSE (IF "x%QUERY%" == "x3" (
-	CALL :FIXMAT %DIR_SFM_TF2C%
+	CALL :FIXMAT "%DIR_SFM_TF2C%"
 ) ELSE (
 	TITLE TF2C_SFM - ERROR
 	ECHO Aborting...
